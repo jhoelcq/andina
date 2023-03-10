e1d37f369d9f695b1c8dca771af11109dbfc990e
Fran <francisco.rios@nativosdigitales.pe>
2023-03-10 10:38:32 -0500

    feat: my news

--- News/MyNewsViewController.swift
+++ News/MyNewsViewController.swift
@@ -9,10 +9,143 @@ import UIKit
 
 class MyNewsViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    var myNews: [NewsItem] = [
+        NewsItem(
+            intNoticiaId: 460098,
+            intSeccionId: 2,
+            vchTitulo: "Empresarios de EAU conocen prioridad que tienen las inversiones en Perú 1",
+            vchBajada: "",
+            vchDescripcion: "",
+            txtContenido: "",
+            contenidoLimpio: "",
+            vchCreador: "gcanchari",
+            vchNombreSeccion: "Economía",
+            vchUrl: "",
+            dtmFecha: "2013-05-26T00:00:00",
+            vchHora: "17:18",
+            arrFotografias: [
+            Foto(
+                intFotografiaId: 210665,
+                intNoticiaId: 460098,
+                vchLeyenda: "Delegación peruana.",
+                vchUrlImgPequena: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841P.jpg",
+                vchUrlImgMediana: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841M.jpg",
+                vchUrlImgWeb: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841W.jpg")
+            ]
+        ),
+        NewsItem(
+            intNoticiaId: 460098,
+            intSeccionId: 2,
+            vchTitulo: "Empresarios de EAU conocen prioridad que tienen las inversiones en Perú 2",
+            vchBajada: "",
+            vchDescripcion: "",
+            txtContenido: "",
+            contenidoLimpio: "",
+            vchCreador: "gcanchari",
+            vchNombreSeccion: "Economía",
+            vchUrl: "",
+            dtmFecha: "2013-05-26T00:00:00",
+            vchHora: "17:18",
+            arrFotografias: [
+            Foto(
+                intFotografiaId: 210665,
+                intNoticiaId: 460098,
+                vchLeyenda: "Delegación peruana.",
+                vchUrlImgPequena: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841P.jpg",
+                vchUrlImgMediana: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841M.jpg",
+                vchUrlImgWeb: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841W.jpg")
+            ]
+        ),
+        NewsItem(
+            intNoticiaId: 460098,
+            intSeccionId: 2,
+            vchTitulo: "Empresarios de EAU conocen prioridad que tienen las inversiones en Perú 3",
+            vchBajada: "",
+            vchDescripcion: "",
+            txtContenido: "",
+            contenidoLimpio: "",
+            vchCreador: "gcanchari",
+            vchNombreSeccion: "Economía",
+            vchUrl: "",
+            dtmFecha: "2013-05-26T00:00:00",
+            vchHora: "17:18",
+            arrFotografias: [
+            Foto(
+                intFotografiaId: 210665,
+                intNoticiaId: 460098,
+                vchLeyenda: "Delegación peruana.",
+                vchUrlImgPequena: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841P.jpg",
+                vchUrlImgMediana: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841M.jpg",
+                vchUrlImgWeb: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841W.jpg")
+            ]
+        ),
+        NewsItem(
+            intNoticiaId: 460098,
+            intSeccionId: 2,
+            vchTitulo: "Empresarios de EAU conocen prioridad que tienen las inversiones en Perú 4",
+            vchBajada: "",
+            vchDescripcion: "",
+            txtContenido: "",
+            contenidoLimpio: "",
+            vchCreador: "gcanchari",
+            vchNombreSeccion: "Economía",
+            vchUrl: "",
+            dtmFecha: "2013-05-26T00:00:00",
+            vchHora: "17:18",
+            arrFotografias: [
+            Foto(
+                intFotografiaId: 210665,
+                intNoticiaId: 460098,
+                vchLeyenda: "Delegación peruana.",
+                vchUrlImgPequena: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841P.jpg",
+                vchUrlImgMediana: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841M.jpg",
+                vchUrlImgWeb: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841W.jpg")
+            ]
+        ),
+        NewsItem(
+            intNoticiaId: 460098,
+            intSeccionId: 2,
+            vchTitulo: "Empresarios de EAU conocen prioridad que tienen las inversiones en Perú 5",
+            vchBajada: "",
+            vchDescripcion: "",
+            txtContenido: "",
+            contenidoLimpio: "",
+            vchCreador: "gcanchari",
+            vchNombreSeccion: "Economía",
+            vchUrl: "",
+            dtmFecha: "2013-05-26T00:00:00",
+            vchHora: "17:18",
+            arrFotografias: [
+            Foto(
+                intFotografiaId: 210665,
+                intNoticiaId: 460098,
+                vchLeyenda: "Delegación peruana.",
+                vchUrlImgPequena: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841P.jpg",
+                vchUrlImgMediana: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841M.jpg",
+                vchUrlImgWeb: "https://portal.andina.pe/EDPFotografia3/Thumbnail/2019/01/09/000554841W.jpg")
+            ]
+        )
+    ]
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var myNewsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        myNewsTableView.dataSource = self
+        myNewsTableView.delegate = self
+        
+        let nib = UINib(nibName: "MyNewsTableViewCell", bundle: nil)
+        
+        myNewsTableView.register(nib, forCellReuseIdentifier: "MyNewsTableViewCell")
+        // relatedNews =
+        myNewsTableView.reloadData()
     }
     
 
@@ -26,4 +159,47 @@ class MyNewsViewController: UIViewController {
     }
     */
 
+}
+
+extension MyNewsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myNews.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyNewsTableViewCell", for: indexPath) as! MyNewsTableViewCell
+        
+        cell.selectionStyle = .none
+            
+        // Configura la celda con los datos correspondientes
+        let newsItem = myNews[indexPath.row]
+        
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        if let date = dateFormatter.date(from: newsItem.dtmFecha) {
+            dateFormatter.dateFormat = "dd 'de' MMMM, yyyy"
+            cell.dateTimeLabel.text = "\(newsItem.vchHora) - \(dateFormatter.string(from: date))"
+        }
+        
+        cell.titleLabel.text = newsItem.vchTitulo
+        
+        if let url = URL(string: newsItem.arrFotografias[0].vchUrlImgMediana) {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data, let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        cell.thumbnail.image = image
+                    }
+                }
+            }
+            task.resume()
+        }
+
+   
+        return cell
+    }
+}
+
+extension MyNewsViewController: UITableViewDelegate {
+    
 }
